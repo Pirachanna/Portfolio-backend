d22dd2334d8cc2267dee84c3fcf02ba7e563e755
Ma. Su. Pirachanna <pirachanna0504@gmail.com>
2025-07-18 22:12:46 +0530

    Fix: corrected import os syntax

--- project/settings.py
+++ project/settings.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import osgit add project/settings.py
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
